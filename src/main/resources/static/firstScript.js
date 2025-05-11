ce8a88c9f79c818935ca755b178c5e64b44e30a1
pschre56 <wi23b079@technikum-wien.at>
2025-05-11 14:00:25 +0200

    cart modiefied

--- src/main/resources/static/firstScript.js
+++ src/main/resources/static/firstScript.js
@@ -21,17 +21,21 @@ function loadCart() {
         .then(res => res.json())
         .then(cart => {
             let html = '';
+            let sum=0;
             if (cart.length === 0) {
                 html = '<p>Dein Warenkorb ist leer.</p>';
             } else {
                 cart.forEach(item => {
                     html += `<p><strong>${item.name}</strong> - ${item.price} €</p>`;
+                    sum+=item.price;
                 });
             }
             document.getElementById('cartContent').innerHTML = html;
+            document.getElementById('cartSum').innerHTML = sum;
             openCartModal();
         })
         .catch(err => console.error('Fehler beim Laden des Warenkorbs:', err));
+
 }
 
 function addToCart(id) {
